58c8660af17749c21a58cfc85c6741d2d41f26ab
Brayan Giovanni Delion Martínez <103006766+DelionM@users.noreply.github.com>
2023-08-10 10:55:12 -0600

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,41 @@
+let output = document.getElementById('output');
+let percentageInput = document.getElementById('percentageInput');
+let resultWithPercentage = document.getElementById('resultWithPercentage');
+let currentInput = '';
+
+function appendToOutput(value) {
+    currentInput += value;
+    output.value = currentInput;
+}
+
+function clearOutput() {
+    currentInput = '';
+    output.value = '';
+    resultWithPercentage.value = '';
+}
+
+function calculate() {
+    try {
+        let result = eval(currentInput);
+        let percentage = parseFloat(percentageInput.value);
+        
+        if (isNaN(percentage) || percentage < 1 || percentage > 10) {
+            resultWithPercentage.value = 'Porcentaje inválido';
+            return;
+        }
+
+        let resultWithPercent = result + (result * percentage / 100);
+        output.value = currentInput;
+        resultWithPercentage.value = resultWithPercent.toFixed(2);
+        currentInput = '';
+    } catch (error) {
+        resultWithPercentage.value = 'Error';
+        currentInput = '';
+    }
+}
+
+
+function backspace() {
+    currentInput = currentInput.slice(0, -1);
+    output.value = currentInput;
+}
